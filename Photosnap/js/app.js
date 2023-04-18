13640e501ea202347224d4a1926fead568e5f4f8
tusatan <130284187+tusatan@users.noreply.github.com>
2023-04-18 17:12:46 +0530

    Add files via upload

---
+++ Photosnap/js/app.js
@@ -0,0 +1,21 @@
+// MOBILE NAVIGATION MENU
+const menuButton = document.querySelector('.menu_toggle');
+const open = document.querySelector('.open');
+const close = document.querySelector('.close');
+const navList = document.querySelector('.nav_list');
+
+menuButton.addEventListener('click', function () {
+    navList.classList.toggle('active');
+    open.classList.toggle('active');
+    close.classList.toggle('active');
+});
+
+// CLOSE THE NAV WHEN NAVLNKS ARE CLICKED
+let navLinks = document.querySelectorAll('.nav_list-link');
+
+navLinks.forEach(function (navLink) {
+    navLink.addEventListener('click', function () {
+        navList.classList.remove('active');
+    })
+})
+
